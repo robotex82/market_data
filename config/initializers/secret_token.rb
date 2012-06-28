42ddc6c1bcedf1033137cd9b0cee8c00df8239f4
Roberto Vasquez Angel <robotex@robotex.de>
2012-06-28 21:03:08 +0200

    Added rails application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MarketData::Application.config.secret_token = '6287b82faeb0bf218ac42b318665add81af6fcb4d5bda38aed63d3d80dcc6187e05031eedd8352be1e5fafed8927c57c9dd8a42ec4f176dbec4cabcab57efde2'
